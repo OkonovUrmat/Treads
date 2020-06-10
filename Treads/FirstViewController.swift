ed7fd2f34f4f094dcab1241c3e89fc2a05dfb0cd
OkonovUrmat <okonov.okonov@gmail.com>
2020-06-10 20:20:28 +0600

    Initial Commit

---
+++ Treads/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treads
+//
+//  Created by Urmat on 6/10/20.
+//  Copyright © 2020 Urmat. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by Urmat on 6/10/20.
+//  Copyright © 2020 Urmat. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
