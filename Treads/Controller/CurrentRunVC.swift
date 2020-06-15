e755f9b03310e27d9cbb54cafbfe760d2623bf08
OkonovUrmat <okonov.okonov@gmail.com>
2020-06-15 19:05:53 +0600

    Make connections between Main.storyboard and CurrentRunVC

--- Treads/Controller/CurrentRunVC.swift
+++ Treads/Controller/CurrentRunVC.swift
@@ -10,21 +10,12 @@ import UIKit
 
 class CurrentRunVC: UIViewController {
 
+    @IBOutlet weak var swipeBGImageVIew: UIImageView!
+    @IBOutlet weak var sliderImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
