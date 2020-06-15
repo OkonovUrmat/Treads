c67b9f286815954c0b5e0f9ad77a9986588bff37
OkonovUrmat <okonov.okonov@gmail.com>
2020-06-15 21:20:53 +0600

    [WIP] CurrentRunVC: Create a slide animation

--- Treads/Controller/CurrentRunVC.swift
+++ Treads/Controller/CurrentRunVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CurrentRunVC: UIViewController {
+class CurrentRunVC: LocationVC {
 
     @IBOutlet weak var swipeBGImageVIew: UIImageView!
     @IBOutlet weak var sliderImageView: UIImageView!
@@ -16,6 +16,25 @@ class CurrentRunVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped(sender:)))
+        sliderImageView.addGestureRecognizer(swipeGesture)
+        sliderImageView.isUserInteractionEnabled = true
+        swipeGesture.delegate = self as? UIGestureRecognizerDelegate
     }
 
+    @objc func endRunSwiped(sender: UIPanGestureRecognizer) {
+        let minAdjust: CGFloat = 80
+        let maxAdjust: CGFloat = 130
+        if let sliderView = sender.view {
+            if sender.state == UIGestureRecognizer.State.began || sender.state == UIGestureRecognizer.State.changed {
+                let translation = sender.translation(in: self.view)
+                if sliderView.center.x >= (swipeBGImageVIew.center.x - minAdjust) && sliderView.center.x <= (swipeBGImageVIew.center.x + maxAdjust) {
+                    sliderView.center = CGPoint(x: sliderView.center.x + translation.x, y: sliderView.center.y)
+                }
+                
+                sender.setTranslation(CGPoint.zero, in: self.view)
+            }
+        }
+    }
+    
 }
