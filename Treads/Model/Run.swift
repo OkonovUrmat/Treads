fb2a258f4b7a1ece37577a4a506abfbe44e0ebbd
OkonovUrmat <okonov.okonov@gmail.com>
2020-07-28 20:02:01 +0600

    Add Run.swift

---
+++ Treads/Model/Run.swift
@@ -0,0 +1,14 @@
+//
+//  Run.swift
+//  Treads
+//
+//  Created by Urmat on 7/28/20.
+//  Copyright © 2020 Urmat. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Run: Object {
+    
+}
