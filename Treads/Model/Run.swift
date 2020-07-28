9841f06f1169d6ad539ed1e6112159c483dc927a
OkonovUrmat <okonov.okonov@gmail.com>
2020-07-28 20:11:40 +0600

    Override functions and init variables

--- Treads/Model/Run.swift
+++ Treads/Model/Run.swift
@@ -14,5 +14,22 @@ class Run: Object {
     dynamic public private(set) var date = NSDate()
     dynamic public private(set) var pace = 0
     dynamic public private(set) var distance = 0.0
-    dynamic public private(set) var duration = 0.0
+    dynamic public private(set) var duration = 0
+    
+    override class func primaryKey() -> String {
+        return "id"
+    }
+    
+    override class func indexedProperties() -> [String] {
+        return ["pace", "date", "duration"]
+    }
+    
+    convenience init(pace: Int, distance: Double, duration: Int) {
+        self.init()
+        self.id = UUID().uuidString.lowercased()
+        self.date = NSDate()
+        self.pace = pace
+        self.distance = distance
+        self.duration = duration
+    }
 }
