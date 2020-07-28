821624adfacd6e09a2b45a3643fade9da00eaf61
OkonovUrmat <okonov.okonov@gmail.com>
2020-07-28 20:05:55 +0600

    Create parameters of Realm

--- Treads/Model/Run.swift
+++ Treads/Model/Run.swift
@@ -10,5 +10,9 @@ import Foundation
 import RealmSwift
 
 class Run: Object {
-    
+    dynamic public private(set) var id = ""
+    dynamic public private(set) var date = NSDate()
+    dynamic public private(set) var pace = 0
+    dynamic public private(set) var distance = 0.0
+    dynamic public private(set) var duration = 0.0
 }
