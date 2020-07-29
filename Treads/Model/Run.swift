589f8b95eb45423f4f3384e2f7317681b031a258
OkonovUrmat <okonov.okonov@gmail.com>
2020-07-29 20:15:21 +0600

    Add run data to Realm

--- Treads/Model/Run.swift
+++ Treads/Model/Run.swift
@@ -34,6 +34,15 @@ class Run: Object {
     }
     
     static func addRunToRealm(pace: Int, distance: Double, duration: Int) {
-        
+        let run = Run(pace: pace, distance: distance, duration: duration)
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(run)
+                try realm.commitWrite()
+            }
+        } catch {
+            debugPrint("Error adding run to Realm!")
+        }
     }
 }
