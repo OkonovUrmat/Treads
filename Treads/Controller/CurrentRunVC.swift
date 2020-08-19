ffe78300169677887e67b07a051f0da583bdf7bd
OkonovUrmat <okonov.okonov@gmail.com>
2020-08-19 14:45:51 +0600

    CurrentRunVC: Add location when run

--- Treads/Controller/CurrentRunVC.swift
+++ Treads/Controller/CurrentRunVC.swift
@@ -8,31 +8,33 @@
 
 import UIKit
 import MapKit
+import RealmSwift
 
 class CurrentRunVC: LocationVC {
 
-    @IBOutlet weak var swipeBGImageVIew: UIImageView!
+    @IBOutlet weak var swipeBGImageView: UIImageView!
     @IBOutlet weak var sliderImageView: UIImageView!
     @IBOutlet weak var durationLbl: UILabel!
     @IBOutlet weak var paceLbl: UILabel!
     @IBOutlet weak var distanceLbl: UILabel!
     @IBOutlet weak var pauseBtn: UIButton!
     
-    var startLocation: CLLocation!
-    var lastLocation: CLLocation!
-    var timer = Timer()
-    
-    var runDistance = 0.0
-    var pace = 0
-    var counter = 0
+    fileprivate var startLocation: CLLocation!
+    fileprivate var lastLocation: CLLocation!
+    fileprivate var timer = Timer()
+    fileprivate var coordinateLocations = List<Location>()
+    fileprivate var runDistance = 0.0
+    fileprivate var pace = 0
+    fileprivate var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped(sender:)))
         sliderImageView.addGestureRecognizer(swipeGesture)
         sliderImageView.isUserInteractionEnabled = true
         swipeGesture.delegate = self as? UIGestureRecognizerDelegate
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,10 +48,10 @@ class CurrentRunVC: LocationVC {
         startTimer()
         pauseBtn.setImage(#imageLiteral(resourceName: "pauseButton"), for: .normal)
     }
-
+    
     func endRun() {
         manager?.stopUpdatingLocation()
-        Run.addRunToRealm(pace: pace, distance: runDistance, duration: counter)
+        Run.addRunToRealm(pace: pace, distance: runDistance, duration: counter, locations: coordinateLocations)
     }
     
     func pauseRun() {
@@ -59,18 +61,18 @@ class CurrentRunVC: LocationVC {
         manager?.stopUpdatingLocation()
         pauseBtn.setImage(#imageLiteral(resourceName: "resumeButton"), for: .normal)
     }
-
+    
     func startTimer() {
         durationLbl.text = counter.formatTimeDurationToString()
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
-    @objc func updateCounter() {
+    @objc func updateCounter(){
         counter += 1
         durationLbl.text = counter.formatTimeDurationToString()
     }
     
-    func calculatePace(time seconds: Int, miles: Double) -> String {
+    func calculatePace(time seconds: Int, miles: Double) -> String{
         pace = Int(Double(seconds) / miles)
         return pace.formatTimeDurationToString()
     }
@@ -89,31 +91,30 @@ class CurrentRunVC: LocationVC {
         if let sliderView = sender.view {
             if sender.state == UIGestureRecognizer.State.began || sender.state == UIGestureRecognizer.State.changed {
                 let translation = sender.translation(in: self.view)
-                if sliderView.center.x >= (swipeBGImageVIew.center.x - minAdjust) && sliderView.center.x <= (swipeBGImageVIew.center.x + maxAdjust) {
+                if sliderView.center.x >= (swipeBGImageView.center.x - minAdjust) && sliderView.center.x <= (swipeBGImageView.center.x + maxAdjust) {
                     sliderView.center.x = sliderView.center.x + translation.x
-                } else if sliderView.center.x >= (swipeBGImageVIew.center.x + maxAdjust){
-                    sliderView.center.x = swipeBGImageVIew.center.x + maxAdjust
+                } else if sliderView.center.x >= (swipeBGImageView.center.x + maxAdjust) {
+                    sliderView.center.x = swipeBGImageView.center.x + maxAdjust
                     endRun()
                     dismiss(animated: true, completion: nil)
                 } else {
-                    sliderView.center.x = swipeBGImageVIew.center.x - minAdjust
+                    sliderView.center.x = swipeBGImageView.center.x - minAdjust
                 }
                 
                 sender.setTranslation(CGPoint.zero, in: self.view)
             } else if sender.state == UIGestureRecognizer.State.ended {
-                UIView.animate(withDuration: 0.1) {
-                    sliderView.center.x = self.swipeBGImageVIew.center.x - minAdjust
-                }
+                UIView.animate(withDuration: 0.1, animations: {
+                    sliderView.center.x = self.swipeBGImageView.center.x - minAdjust
+                })
             }
         }
     }
-    
 }
 
 extension CurrentRunVC: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse {
-            checkLocationStatus()
+            checkLocationAuthStatus()
         }
     }
     
@@ -122,12 +123,13 @@ extension CurrentRunVC: CLLocationManagerDelegate {
             startLocation = locations.first
         } else if let location = locations.last {
             runDistance += lastLocation.distance(from: location)
+            let newLocation = Location(latitude: Double(lastLocation.coordinate.latitude), longitude: Double(lastLocation.coordinate.longitude))
+            coordinateLocations.insert(newLocation, at: 0)
             distanceLbl.text = "\(runDistance.metersToMiles(places: 2))"
             if counter > 0 && runDistance > 0 {
                 paceLbl.text = calculatePace(time: counter, miles: runDistance.metersToMiles(places: 2))
             }
         }
-        
         lastLocation = locations.last
     }
 }
